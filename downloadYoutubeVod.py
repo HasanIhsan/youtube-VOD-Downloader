f98518b384b990aa8153e59046cb3ed59596c105
Hassan I <ihsanhassan.business@gmail.com>
2023-02-22 13:36:38 -0500

    update: added working with text file

--- downloadYoutubeVod.py
+++ downloadYoutubeVod.py
@@ -1,14 +1,51 @@
 from pytube import YouTube
+from pytube import Playlist
+from pathlib import Path
+import os
 
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
+#using pytube to download playlists (currently not implmented with my code)
+# needs testing! 
+def DownloadPlaylists(link):
+    youtubeObject = Playlist(link)
+
     try:
-        youtubeObject.download()
+        #printing video titles
+        print(f'Downloading: {youtubeObject.title}') 
+
+        os.mkdir('Downloaded_Video_Playlist')
+        for video in youtubeObject.videos:
+            video.streams.first().download(os.getcwd()+'\Downloaded_Video_Playlist')
+
+    except:
+        print("an error has Occured")
+    print("Download is Completed successfully")
+
+
+#using pytube to download videos from a linkb
+def DownloadVideo(link):
+    #create a youtube object for youtube
+    #get the highest resolution video has (may take a long time to download for larger resolutions)
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_by_itag(22)
+    try:
+        os.mkdir('Downloaded_Videos') 
+        youtubeObject.download(os.getcwd()+'\Downloaded_Videos')
     except:
         print("An error has occurred")
     print("Download is completed successfully")
 
 
-link = input("Enter the YouTube video URL: ")
-Download(link)
+
+#Using readlines() read a file 
+links = open(os.getcwd()+"\links.txt") 
+AllLinks = links.readlines()
+  
+count = 0
+# Strips the newline character
+for link in AllLinks:
+    count += 1
+    print("Link: {}: {}".format(count, link.strip()))
+    DownloadVideo(link)
+
+ 
+ 
