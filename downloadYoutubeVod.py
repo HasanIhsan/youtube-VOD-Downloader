2a76adfc1b8fb407d1009fb4148b36d0b30d6dce
Hassan I <ihsanhassan.business@gmail.com>
2023-02-23 13:32:52 -0500

    update: fixed errors (now prop owking)

--- downloadYoutubeVod.py
+++ downloadYoutubeVod.py
@@ -21,14 +21,14 @@ def DownloadPlaylists(link):
     print("Download is Completed successfully")
 
 
-#using pytube to download videos from a linkb
+#using pytube to download videos from a link
 def DownloadVideo(link):
     #create a youtube object for youtube
     #get the highest resolution video has (may take a long time to download for larger resolutions)
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_by_itag(22)
     try:
-        os.mkdir('Downloaded_Videos') 
+       
         youtubeObject.download(os.getcwd()+'\Downloaded_Videos')
     except:
         print("An error has occurred")
@@ -39,9 +39,12 @@ def DownloadVideo(link):
 #Using readlines() read a file 
 links = open(os.getcwd()+"\links.txt") 
 AllLinks = links.readlines()
-  
+
+#Create a new folder called downloaded_videos
+os.mkdir('Downloaded_Videos')   
 count = 0
 # Strips the newline character
+
 for link in AllLinks:
     count += 1
     print("Link: {}: {}".format(count, link.strip()))
